f598fab917846fe10cf7dfc9c2c580ccf6f838fb
zhezhaoa <1152543959@qq.com>
2017-08-24 16:06:22 +0800

    Add files via upload

--- bi_bi.sh
+++ bi_bi.sh
@@ -1,47 +1,43 @@
 #!/bin/sh
 
-win=5
+win=2
 size=300
 thr=10
 sub=1e-5
 iters=3
 threads=10
 negative=5
-corpus=wiki.en.txt
-output_path=outputs/bi_bi
+memsize=8.0
+corpus=wiki2010.clean
+output_path=outputs/bi_bi/win${win}
 
-mkdir -p outputs/bi_bi/win${win}/sgns
-python hyperwords/corpus2vocab.py --ngram 2 --reduce_thr 30000000 ${corpus} ${output_path}/win${win}/vocab
-python hyperwords/corpus2pairs.py --win ${win} --sub ${sub} --ngram_word 2 --ngram_context 2 --thr ${thr} --threads ${threads} ${corpus} ${output_path}/win${win}/vocab ${output_path}/win${win}/pairs
-
+mkdir -p ${output_path}/sgns
+#python ngram2vec/corpus2vocab.py --ngram 2 --memory_size ${memsize} --min_count ${thr} ${corpus} ${output_path}/vocab
+#python ngram2vec/corpus2pairs.py --win ${win} --sub ${sub} --ngram_word 2 --ngram_context 2 --threads_num ${threads} --overlap ${corpus} ${output_path}/vocab ${output_path}/pairs
 if [ -f "${output_path}/win${win}/pairs" ]
 then
-	rm ${output_path}/win${win}/pairs
+	rm ${output_path}/pairs
 fi
 for i in $(seq 0 $((${threads}-1)) )
 do
-	cat ${output_path}/win${win}/pairs_${i} >> ${output_path}/win${win}/pairs
-	rm ${output_path}/win${win}/pairs_${i}
+	cat ${output_path}/pairs_${i} >> ${output_path}/pairs
+	rm ${output_path}/pairs_${i}
 done
+python ngram2vec/pairs2vocab.py ${output_path}/pairs ${output_path}/words.vocab ${output_path}/contexts.vocab
 
-python hyperwords/pairs2vocab.py ${output_path}/win${win}/pairs ${output_path}/win${win}/sgns/sgns.words.vocab ${output_path}/win${win}/sgns/sgns.contexts.vocab
-./word2vecf/word2vecf -train ${output_path}/win${win}/pairs -pow 0.75 -cvocab ${output_path}/win${win}/sgns/sgns.contexts.vocab -wvocab ${output_path}/win${win}/sgns/sgns.words.vocab -dumpcv ${output_path}/win${win}/sgns/sgns.contexts -output ${output_path}/win${win}/sgns/sgns.words -threads ${threads} -negative ${negative} -size ${size} -iters ${iters}
-
-
-python hyperwords/text2numpy.py ${output_path}/win${win}/sgns/sgns.words
-python hyperwords/text2numpy.py ${output_path}/win${win}/sgns/sgns.contexts
-
-python hyperwords/analogy_eval.py SGNS ${output_path}/win${win}/sgns/sgns testsets/analogy/google.txt
-python hyperwords/analogy_eval.py SGNS ${output_path}/win${win}/sgns/sgns testsets/analogy/semantic.txt
-python hyperwords/analogy_eval.py SGNS ${output_path}/win${win}/sgns/sgns testsets/analogy/syntactic.txt
-python hyperwords/analogy_eval.py SGNS ${output_path}/win${win}/sgns/sgns testsets/analogy/msr.txt
-
-python hyperwords/ws_eval.py SGNS ${output_path}/win${win}/sgns/sgns testsets/ws/ws353_similarity.txt
-python hyperwords/ws_eval.py SGNS ${output_path}/win${win}/sgns/sgns testsets/ws/ws353_relatedness.txt
-python hyperwords/ws_eval.py SGNS ${output_path}/win${win}/sgns/sgns testsets/ws/bruni_men.txt
-python hyperwords/ws_eval.py SGNS ${output_path}/win${win}/sgns/sgns testsets/ws/radinsky_mturk.txt
-python hyperwords/ws_eval.py SGNS ${output_path}/win${win}/sgns/sgns testsets/ws/luong_rare.txt
-python hyperwords/ws_eval.py SGNS ${output_path}/win${win}/sgns/sgns testsets/ws/sim999.txt
+./word2vecf/word2vecf -train ${output_path}/pairs -pow 0.75 -cvocab ${output_path}/contexts.vocab -wvocab ${output_path}/words.vocab -dumpcv ${output_path}/sgns/sgns.contexts -output ${output_path}/sgns/sgns.words -threads ${threads} -negative ${negative} -size ${size} -iters ${iters}
 
 
+python ngram2vec/text2numpy.py ${output_path}/sgns/sgns.words
+python ngram2vec/text2numpy.py ${output_path}/sgns/sgns.contexts
+analogy_path = testsets/analogy
+for dataset in ${analogy_path}/google.txt ${analogy_path}/semantic.txt ${analogy_path}/syntactic.txt ${analogy_path}/msr.txt
+do
+	python ngram2vec/analogy_eval.py SGNS ${output_path}/sgns/sgns ${dataset}
+done
+ws_path = testsets/ws
+for dataset in ${ws_path}/ws353_similarity.txt ${ws_path}/ws353_relatedness.txt ${ws_path}/bruni_men.txt ${ws_path}/radinsky_mturk.txt ${ws_path}/luong_rare.txt ${ws_path}/sim999.txt
+do
+	python ngram2vec/ws_eval.py SGNS ${output_path}/sgns/sgns ${dataset}
+done
 
--- uni_bi.sh
+++ uni_bi.sh
@@ -5,17 +5,17 @@ size=300
 thr=10
 sub=1e-5
 iters=3
-threads=12
+threads=10
 negative=5
 memsize=8.0
 corpus=wiki2010.clean
-output_path=outputs/uni_uni/win${win}
+output_path=outputs/uni_bi/win${win}
 
 mkdir -p ${output_path}/sgns
 mkdir -p ${output_path}/ppmi
-python ngram2vec/corpus2vocab.py --ngram 1 --memory_size ${memsize} --min_count ${thr} ${corpus} ${output_path}/vocab
-python ngram2vec/corpus2pairs.py --win ${win} --sub ${sub} --ngram_word 1 --ngram_context 1 --threads_num ${threads} ${corpus} ${output_path}/vocab ${output_path}/pairs
-#concatenate pair files 
+python ngram2vec/corpus2vocab.py --ngram 2 --memory_size ${memsize} --min_count ${thr} ${corpus} ${output_path}/vocab
+python ngram2vec/corpus2pairs.py --win ${win} --sub ${sub} --ngram_word 1 --ngram_context 2 --threads_num ${threads} --overlap ${corpus} ${output_path}/vocab ${output_path}/pairs
+#concatenate pair files
 if [ -f "${output_path}/pairs" ]
 then
 	rm ${output_path}/pairs
@@ -25,13 +25,10 @@ do
 	cat ${output_path}/pairs_${i} >> ${output_path}/pairs
 	rm ${output_path}/pairs_${i}
 done
-#generate (center) word vocabulary and context vocabulary, which are used as vocabulary files for all models
 python ngram2vec/pairs2vocab.py ${output_path}/pairs ${output_path}/words.vocab ${output_path}/contexts.vocab
 
-#SGNS, learn representation upon pairs
 ./word2vecf/word2vecf -train ${output_path}/pairs -pow 0.75 -cvocab ${output_path}/contexts.vocab -wvocab ${output_path}/words.vocab -dumpcv ${output_path}/sgns/sgns.contexts -output ${output_path}/sgns/sgns.words -threads ${threads} -negative ${negative} -size ${size} -iters ${iters}
 
-#SGNS evaluation
 python ngram2vec/text2numpy.py ${output_path}/sgns/sgns.words
 python ngram2vec/text2numpy.py ${output_path}/sgns/sgns.contexts
 for dataset in testsets/analogy/google.txt testsets/analogy/semantic.txt testsets/analogy/syntactic.txt testsets/analogy/msr.txt
@@ -43,13 +40,9 @@ do
 	python ngram2vec/ws_eval.py SGNS ${output_path}/sgns/sgns ${dataset}
 done
 
-#generate co-occurrence matrix from pairs
-python ngram2vec/pairs2counts.py --memory_size ${memsize} ${output_path}/pairs ${output_path}/words.vocab ${output_path}/contexts.vocab ${output_path}/counts
-
-#PPMI, learn representation upon counts (co-occurrence matrix)
+python ngram2vec/pairs2counts.py --memory_size ${memsize} ${output_path}/pairs ${output_path}/words.vocab ${output_path}/contexts.vocab ${output_path}/counts 
 python ngram2vec/counts2pmi.py ${output_path}/words.vocab ${output_path}/contexts.vocab ${output_path}/counts ${output_path}/ppmi/ppmi
 
-#PPMI evaluation
 for dataset in testsets/analogy/google.txt testsets/analogy/semantic.txt testsets/analogy/syntactic.txt testsets/analogy/msr.txt
 do
 	python ngram2vec/analogy_eval.py PPMI ${output_path}/ppmi/ ${dataset}
