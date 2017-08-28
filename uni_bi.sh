f187dee2f44c574975b913be8769966dbde2b1f7
zhezhaoa <1152543959@qq.com>
2017-08-28 23:28:08 +0800

    Add files via upload

--- uni_bi.sh
+++ uni_bi.sh
@@ -14,6 +14,7 @@ output_path=outputs/uni_bi/win${win}
 mkdir -p ${output_path}/sgns
 mkdir -p ${output_path}/ppmi
 mkdir -p ${output_path}/svd
+mkdir -p ${output_path}/glove
 python ngram2vec/corpus2vocab.py --ngram 2 --memory_size ${memsize} --min_count ${thr} ${corpus} ${output_path}/vocab
 python ngram2vec/corpus2pairs.py --win ${win} --sub ${sub} --ngram_word 1 --ngram_context 2 --threads_num ${threads} --overlap ${corpus} ${output_path}/vocab ${output_path}/pairs
 if [ -f "${output_path}/pairs" ]
@@ -27,6 +28,7 @@ do
 done
 python ngram2vec/pairs2vocab.py ${output_path}/pairs ${output_path}/words.vocab ${output_path}/contexts.vocab
 
+#SGNS
 ./word2vecf/word2vecf -train ${output_path}/pairs -pow 0.75 -cvocab ${output_path}/contexts.vocab -wvocab ${output_path}/words.vocab -dumpcv ${output_path}/sgns/sgns.contexts -output ${output_path}/sgns/sgns.words -threads ${threads} -negative ${negative} -size ${size} -iters ${iters}
 
 #SGNS evaluation
@@ -43,7 +45,8 @@ do
 	python ngram2vec/ws_eval.py SGNS ${output_path}/sgns/sgns ${dataset}
 done
 
-python ngram2vec/pairs2counts.py --memory_size ${memsize} ${output_path}/pairs ${output_path}/words.vocab ${output_path}/contexts.vocab ${output_path}/counts 
+python ngram2vec/pairs2counts.py --memory_size ${memsize} ${output_path}/pairs ${output_path}/words.vocab ${output_path}/contexts.vocab ${output_path}/counts
+#PPMI
 python ngram2vec/counts2ppmi.py ${output_path}/words.vocab ${output_path}/contexts.vocab ${output_path}/counts ${output_path}/ppmi/ppmi
 
 #PPMI evaluation
@@ -58,6 +61,7 @@ do
 	python ngram2vec/ws_eval.py PPMI ${output_path}/ppmi/ppmi ${dataset}
 done
 
+#SVD
 python ngram2vec/ppmi2svd.py ${output_path}/ppmi/ppmi ${output_path}/svd/svd 
 
 #SVD evaluation
@@ -70,4 +74,22 @@ done
 for dataset in testsets/ws/ws353_similarity.txt testsets/ws/ws353_relatedness.txt testsets/ws/bruni_men.txt testsets/ws/radinsky_mturk.txt testsets/ws/luong_rare.txt testsets/ws/sim999.txt
 do
 	python ngram2vec/ws_eval.py SVD ${output_path}/svd/svd ${dataset}
+done
+
+#GloVe
+python ngram2vec/counts2shuf.py ${output_path}/counts ${output_path}/counts.shuf
+python ngram2vec/counts2bin.py ${output_path}/counts.shuf ${output_path}/counts.shuf.bin
+
+./GloVe/build/glove -save-file ${output_path}/glove/glove.words -threads ${threads} -input-file ${output_path}/counts.shuf.bin -vector-size ${size} -words-file ${output_path}/words.vocab -contexts-file ${output_path}/contexts.vocab 
+
+cp ${output_path}/words.vocab ${output_path}/glove/glove.words.vocab
+python ngram2vec/text2numpy.py ${output_path}/glove/glove.words
+#GloVe evaluation
+for dataset in testsets/analogy/google.txt testsets/analogy/semantic.txt testsets/analogy/syntactic.txt testsets/analogy/msr.txt
+do
+	python ngram2vec/analogy_eval.py GLOVE ${output_path}/glove/glove ${dataset}
+done
+for dataset in testsets/ws/ws353_similarity.txt testsets/ws/ws353_relatedness.txt testsets/ws/bruni_men.txt testsets/ws/radinsky_mturk.txt testsets/ws/luong_rare.txt testsets/ws/sim999.txt
+do
+	python ngram2vec/ws_eval.py GLOVE ${output_path}/glove/glove ${dataset}
 done
