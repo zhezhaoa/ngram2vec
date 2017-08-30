da6f263ed31dbbdc2ff568922cb793e6e0943722
zhezhaoa <1152543959@qq.com>
2017-08-30 10:50:16 +0800

    Add files via upload

--- ngram2vec/counts2shuf.py
+++ ngram2vec/counts2shuf.py
@@ -2,6 +2,8 @@ from docopt import docopt
 from scipy.sparse import dok_matrix, csr_matrix
 import numpy as np
 import random
+from sys import getsizeof
+import os
 
 from representations.matrix_serializer import save_matrix, save_vocabulary, load_vocabulary
 
@@ -12,25 +14,82 @@ def main():
         counts2shuf.py [options] <counts> <output>
     
     Options:
-        --memory_size NUM        Memory size available [default: 8.0]
+        --memory_size NUM        Memory size available [default: 2.0]
     """)
     
     print "**********************"
     print "counts2shuf"
 
+    #shuffle round 1
+    memory_size = float(args['--memory_size']) * 1000**3
     counts = []
+    counts_num_per_file = []
+    tmp_id = 0
     with open(args['<counts>'], 'r') as f:
         counts_num = 0
-        print str(counts_num/1000**2) + "M tokens processed."
+        print str(counts_num/1000**2) + "M counts processed."
         for line in f:
-            print "\x1b[1A" + str(counts_num/1000**2) + "M tokens processed."
+            print "\x1b[1A" + str(counts_num/1000**2) + "M counts processed."
             counts_num += 1
             word, context, count = line.strip().split()
-            counts.append((word, context, count))
+            counts.append((int(word), int(context), float(count)))
+            if getsizeof(counts) + (getsizeof((int(0),int(0),float(0))) + getsizeof(int(0)) * 2 + getsizeof(float(0)) ) * len(counts) > memory_size:
+                random.shuffle(counts)
+                with open(args['<output>'] + str(tmp_id), 'w') as f:
+                    for count in counts:
+                        f.write(str(count[0]) + ' ' + str(count[1]) + ' ' + str(count[2]) + '\n')
+                counts_num_per_file.append(counts_num)
+                counts = []
+                tmp_id += 1
+
     random.shuffle(counts)
-    with open(args['<output>'], 'w') as f:
+    with open(args['<output>'] + str(tmp_id), 'w') as f:
         for count in counts:
-            f.write(count[0] + ' ' + count[1] + ' ' + count[2] + '\n')
+            f.write(str(count[0]) + ' ' + str(count[1]) + ' ' + str(count[2]) + '\n')
+        counts = []
+        tmp_id += 1
+
+    print "number of tmpfiles: ", tmp_id 
+
+    #shuffle round 2
+    counts_num = 0
+    output_file = open(args['<output>'], 'w')
+    tmpfiles = []
+    for i in xrange(tmp_id):
+        tmpfiles.append(open(args['<output>'] + str(i), 'r'))
+    
+    tmp_num = counts_num_per_file[0] / tmp_id
+    print str(counts_num/1000**2) + "M counts processed."
+    for i in xrange(tmp_id - 1):
+        counts = []
+        for f in tmpfiles:
+            for j in xrange(tmp_num):
+                line = f.readline()
+                if len(line) > 0:
+                    print "\x1b[1A" + str(counts_num/1000**2) + "M counts processed."
+                    counts_num += 1
+                    word, context, count = line.strip().split()
+                    counts.append((int(word), int(context), float(count)))
+        random.shuffle(counts)
+        for count in counts:
+            output_file.write(str(count[0]) + ' ' + str(count[1]) + ' ' + str(count[2]) + '\n')
+    counts = []
+    for f in tmpfiles:
+        for line in f:
+            print "\x1b[1A" + str(counts_num/1000**2) + "M counts processed."
+            counts_num += 1
+            word, context, count = line.strip().split()
+            counts.append((int(word), int(context), float(count)))
+    random.shuffle(counts)
+    for count in counts:
+        output_file.write(str(count[0]) + ' ' + str(count[1]) + ' ' + str(count[2]) + '\n')
+
+    for i in xrange(tmp_id):
+        tmpfiles[i].close()
+    for i in xrange(tmp_id):
+        os.remove(args['<output>'] + str(i))
+    output_file.close()    
+    print "number of counts: ", counts_num
 
 if __name__ == '__main__':
     main()
